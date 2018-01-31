d67dabdd93d1bcd39ab0e619dff8da5e5f255533
Viraj <viraj@Vishnus-iMac.local>
2018-01-31 19:42:24 +0530

    first commit

---
+++ src/com/viraj/PrimitiveDataTypes/PrimitiveDataType.java
@@ -0,0 +1,57 @@
+package com.viraj.PrimitiveDataTypes;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Primitive data types
+		
+		// byte - 8 bits
+		
+		byte dataNumber = 8;
+		System.out.println(dataNumber);
+		
+		// short - 16 bits
+		
+		short dataNumber2 = 233;
+		System.out.println(dataNumber2);
+		
+		// int - 32 bits
+		
+		int dataNumber3 = 4555;
+		System.out.println(dataNumber3);
+		
+		// long - 64 bits
+		
+		long dataNumber4 = 66666;
+		System.out.println(dataNumber4);
+		
+		// float numbers
+		
+		float dataNumber5 = 5.5f;
+		System.out.println(dataNumber5);
+		
+		// double numbers
+		
+		double dataNumber6 = 88.88;
+		System.out.println(dataNumber6);
+		
+		// char - 16 bits
+		
+		char dataNumber7 = 'H';
+		System.out.println(dataNumber7);
+		
+		// boolean true or false 
+		
+		boolean dataNumber8 = true;
+		System.out.println(dataNumber8);
+		
+		// constant make use of final keyword
+		
+		final int number = 800;
+		System.out.println(number);
+		
+	}
+
+}
